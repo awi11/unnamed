e1feb7c8b21965cc56cd05c43dbe152841d18647
Alan Willey <awilley17@gmail.com>
2020-08-08 11:25:25 -0600

    using git with windows what trash is this

---
+++ app/src/main/java/triton/spark/project/CreateUserActivity.java
@@ -0,0 +1,24 @@
+package triton.spark.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class CreateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+
+    public void onClick(View view) {
+        EditText input = (EditText) findViewById(R.id.username);
+        String string = input.getText().toString();
+        Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
+    }
+}
